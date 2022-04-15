323b03b79d3401a5b43b6a2fe18c687108678cb0
Daniel Raffer <daniel.raffer@gmail.com>
2022-04-15 20:45:14 +0200

    move settings to separate component

---
+++ src/components/settings/settings.tsx
@@ -0,0 +1,53 @@
+
+import Button from '@mui/material/Button'
+import Range from '../range/range'
+import React from 'react'
+import { Dispatch } from 'react';
+import { SetStateAction } from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+type SettingsProps = {
+  sorting: boolean;
+  isSorted: boolean;
+  resetApp: () => void;
+  setSortThrottling: Dispatch<SetStateAction<number>>;
+  handleSetSorting: () => void;
+}
+
+const Settings = ({ sorting, isSorted, resetApp, setSortThrottling, handleSetSorting }: SettingsProps) => {
+  return (
+    <Box mt={5}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+      >
+        <Grid container direction="row" justifyContent="center">
+          <Box mr={2}>
+            <Button
+              variant="contained"
+              color={ sorting ? "error": "success"}
+              onClick={handleSetSorting}
+              disabled={isSorted}
+            >
+              {sorting ? "Pause": "Start"}
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={resetApp}
+            >
+              Reset
+            </Button>
+          </Box>
+        </Grid>
+        <Range setSortThrottling={setSortThrottling}/>
+      </Grid>
+    </Box>
+  )
+}
+
+export default Settings
